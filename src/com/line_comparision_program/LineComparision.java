051c04406f6e4cc9f8e14bde52c712eb730c1720
kiranudhan <udhankiran62@gmail.com>
2023-03-11 20:54:57 +0530

    UC1->calculated length of line

--- src/com/line_comparision_program/LineComparision.java
+++ src/com/line_comparision_program/LineComparision.java
@@ -1,7 +1,24 @@
 package com.line_comparision_program;
 
+import java.util.Scanner;
+
 public class LineComparision {
     public static void main(String[] args) {
         System.out.println("Welcome to Line Comparison Computation Program");
+        LineComparision lineComparision = new LineComparision();
+        lineComparision.calculateLength();
+
+    }
+
+    public void calculateLength(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter first co-ordinates vale x1 and y1 is ");
+        int x1 = sc.nextInt();
+        int y1 = sc.nextInt();
+        System.out.println("enter first co-ordinates vale x2 and y2 is");
+        int x2 = sc.nextInt();
+        int y2 = sc.nextInt();
+        double length = Math.sqrt((Math.pow((x2-x1),2)) +(Math.pow((y2-y1),2)));
+        System.out.println("Length of line :"+length);
     }
 }
