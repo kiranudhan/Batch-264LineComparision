72995993e66a995cc3bbc0a7f4d9904337180f8c
kiranudhan <udhankiran62@gmail.com>
2023-03-11 21:11:56 +0530

    UC2->calculated length of two line, check lines are equal

--- src/com/line_comparision_program/LineComparision.java
+++ src/com/line_comparision_program/LineComparision.java
@@ -4,13 +4,6 @@ import java.util.Scanner;
 
 public class LineComparision {
     public static void main(String[] args) {
-        System.out.println("Welcome to Line Comparison Computation Program");
-        LineComparision lineComparision = new LineComparision();
-        lineComparision.calculateLength();
-
-    }
-
-    public void calculateLength(){
         Scanner sc = new Scanner(System.in);
         System.out.println("enter first co-ordinates vale x1 and y1 is ");
         int x1 = sc.nextInt();
@@ -18,7 +11,25 @@ public class LineComparision {
         System.out.println("enter first co-ordinates vale x2 and y2 is");
         int x2 = sc.nextInt();
         int y2 = sc.nextInt();
-        double length = Math.sqrt((Math.pow((x2-x1),2)) +(Math.pow((y2-y1),2)));
-        System.out.println("Length of line :"+length);
+        double length1 = Math.sqrt((Math.pow((x2-x1),2)) +(Math.pow((y2-y1),2)));
+        String len1=Double.toString(length1);
+        System.out.println("enter first co-ordinates vale x3 and y3 is");
+        int x3 = sc.nextInt();
+        int y3 = sc.nextInt();
+        System.out.println("enter first co-ordinates vale x4 and y4 is");
+        int x4 = sc.nextInt();
+        int y4 = sc.nextInt();
+        double length2 = Math.sqrt((Math.pow((x4-x3),2)) + (Math.pow((y4-y3),2)));
+        String len2=Double.toString(length2);
+        System.out.println("length of the first line " +len1);
+        System.out.println("length of the first line " +len2);
+        System.out.println();
+
+        if(len1.equals(len2))
+            System.out.println("Both Lines are EQUAL ");
+        else
+            System.out.println("Both Lines are not EQUAL");
+
     }
+
 }
